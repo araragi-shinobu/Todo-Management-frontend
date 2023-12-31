a5079e8aacf92e78231bf4ce9e881e9e58cbc12a
araragi-shinobu <785712896@qq.com>
2023-12-31 07:38:43 -0800

    Add header and footer

---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className='navbar navbar-expad-md navbar-dark bg-dark'>
+                    <div>
+                        <a href='http://localhost:3000' className='navbar-brand'>
+                            Todo Management Application
+                        </a>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
+}
